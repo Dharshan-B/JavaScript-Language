a7ce9647d0469ee2e49b34f0763cd936d66dfff2
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2025-06-13 12:00:44 +0530

    Create ButtonColorChange.js

---
+++ ButtonColorChange.js
@@ -0,0 +1,7 @@
+<button id="ch" onclick="ChangeColor()">Change</button>
+<script>
+  var ch = document.getElementById("ch");
+function ChangeColor() {
+  ch.style.backgroundColor="red";
+}
+</script>
